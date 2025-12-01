cb61b5098d2f81289deb68473bbd5e63121a60b0
Simone <simone.starinieri02@gmail.com>
2025-12-02 00:36:05 +0100

    first test with cronjob on render

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-import * as  http from 'http';
-import * as fs from 'fs';
-import dateEvents from 'date-events';
-import {getEvents} from './GCal.js';
-import {sendReminder} from './WABusinessAPI.js';
-
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const {getEvents} = require('./GCal.js')
+const {sendReminder} = require('./WABusinessAPI.js');
+const destroyer = require('server-destroy');
+app.use(express.static('public'));
 const timeMin = new Date();
 timeMin.setDate(timeMin.getDate()+1);
 const timeMax = new Date(timeMin);
 timeMin.setHours(0,0,1,0);
 timeMax.setHours(23,59,59,0);
-
-handleMessages().catch(console.error);
-console.log('Messages sent');
-
-async function handleMessages(){
-    const events = await getEvents(timeMin,timeMax);
-    console.log(events);
+app.get('/',async (req,res)=>{
+    console.log('test')
+    res.sendFile(path.join(__dirname,'public/index.html'));
+    let events = await getEvents().catch(console.error);
     await sendReminder(events);
+})
 
-}
+app.listen(port,()=>{
+    console.log('server started');
+})
